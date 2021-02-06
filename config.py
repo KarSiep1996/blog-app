4f454b90b98c48968377fb5e5c1ba0c8e28457d0
Karolina Siepak <karsiep1996@gmail.com>
2021-02-06 18:22:54 +0100

    Modify config.py

--- config.py
+++ config.py
@@ -11,3 +11,9 @@ class Config:
    SQLALCHEMY_TRACK_MODIFICATIONS = False
    ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me")
+   MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
+   MAIL_PORT = os.environ.get("MAIL_PORT", 465)
+   MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "ancymonka1996@gmail.com")
+   MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", "Misiaczek@12")
+   MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", False)
+   MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL", True)
